d205bc86d5f73551645cd514e988ce9bae85a33e
mvmilovanov <mvmilovanow@gmail.com>
2020-06-19 16:44:55 +0700

    working prototype

--- partpicker.py
+++ partpicker.py
@@ -5,10 +5,13 @@ import urllib
 import json
 
 def getpageFromWebsite(website, partname, timeout = 500):
-    fullpath = website + urllib.parse.quote(partname)
-    loaded_page = urllib.request.urlopen(fullpath, timeout=timeout)
-    loaded_page = loaded_page.read()
-    soup = bs4.BeautifulSoup(loaded_page, 'html.parser')
+    try:
+        fullpath = website + urllib.parse.quote(partname)# '+'.join([urllib.parse.quote(x) for x in partname.lower().strip().split(' ')])
+        loaded_page = urllib.request.urlopen(fullpath, timeout=timeout)
+        loaded_page = loaded_page.read()
+        soup = bs4.BeautifulSoup(loaded_page, 'html.parser')
+    except:
+        soup = bs4.BeautifulSoup()
     return soup
 def parseDrom(soup):
     part_frames = [x for x in soup.find_all(class_ = 'descriptionCell bull-item-content__cell bull-item-content__description-cell js-description-block')]
@@ -18,7 +21,7 @@ def parseDrom(soup):
     if len(x.find_all(class_ = 'price-block__price')) > 0 \
         else x.find_all(class_ = 'price-block__final-price finalPrice tooltip-element tooltip-s')[0].get_text() \
     for x in part_frames]
-    return zip(part_names, part_links, part_price)
+    return part_names #zip(part_names, part_links, part_price)
 def parseAuto(soup):
     part_frames = [x for x in soup.find_all(class_ = 'SerpSnippet__data')]
     part_names = [x.find_all(class_ = 'PartsLink PartsLink_color_black PartsLink_padded SerpSnippet__title')[0].get_text() for x in part_frames]
@@ -31,56 +34,45 @@ def getPriceInfo(request_parameters):
     page = req['page']
     webpages = [r'https://parts.auto.ru/zapchasti/?isNew=all&text=', r'https://baza.drom.ru/sell_spare_parts/?query=']
     parts = []
-    for page in webpages:
-        soup = getpageFromWebsite(page)
+    for webpage in webpages:
+        soup = getpageFromWebsite(webpage, part_name)
         parts += [parseWebpage(soup)]
+    # return parts
     result = []
-    
+    entries = max([len(x) for x in parts])
+    step = len(webpages)*3
+    for i in range(0, int(entries/3)+1, 1):
+        for res in parts:
+            result += res[i*3:(i+1)*3]
+    # print(page*step)
+    # print((page+1)*step)
+    return result[page*step:(page+1)*step]
+
 def parseWebpage(soup):
-    if len(soup.findall(class_ = 'js no-main0 drom-notouch')) > 0:
-        part_frames = [x for x in soup.find_all(class_ = 'descriptionCell bull-item-content__cell bull-item-content__description-cell js-description-block')]
-        part_names = [x.find_all(class_ = 'bulletinLink bull-item__self-link auto-shy')[0].get_text() for x in part_frames]
-        part_links = ['https://baza.drom.ru' + x.find_all(class_ = 'bulletinLink bull-item__self-link auto-shy')[0]['href'] for x in part_frames ]
-        part_price = [x.find_all(class_ = 'price-block__price')[0].get_text() \
-        if len(x.find_all(class_ = 'price-block__price')) > 0 \
-            else x.find_all(class_ = 'price-block__final-price finalPrice tooltip-element tooltip-s')[0].get_text() \
-        for x in part_frames]
-        return zip(part_names, part_links, part_price)
-    else:
-         part_frames = [x for x in soup.find_all(class_ = 'SerpSnippet__data')]
-        part_names = [x.find_all(class_ = 'PartsLink PartsLink_color_black PartsLink_padded SerpSnippet__title')[0].get_text() for x in part_frames]
-        part_links = ['https://baza.drom.ru' + x.find_all(class_ = 'PartsLink PartsLink_color_black PartsLink_padded SerpSnippet__title')[0]['href'] for x in part_frames ]
-        part_price = [x.find_all(class_ = 'SerpSnippet__price')[0].get_text() for x in part_frames]
-        return zip(part_names, part_links, part_price)
+    # return(soup)
+    try:
+        if len(soup.find_all(class_ = 'descriptionCell bull-item-content__cell bull-item-content__description-cell js-description-block')) > 0:
+            part_frames = [x for x in soup.find_all(class_ = 'descriptionCell bull-item-content__cell bull-item-content__description-cell js-description-block')]
+            part_names = [x.find_all(class_ = 'bulletinLink bull-item__self-link auto-shy')[0].get_text() for x in part_frames]
+            part_links = ['https://baza.drom.ru' + x.find_all(class_ = 'bulletinLink bull-item__self-link auto-shy')[0]['href'] for x in part_frames ]
+            part_price = [x.find_all(class_ = 'price-per-quantity__price')[0].get_text() \
+            if len(x.find_all(class_ = 'price-per-quantity__price')) > 0 \
+            else (x.find_all(class_ = 'priceCell price-block')[0].get_text() \
+                if len(x.find_all(class_ = 'priceCell price-block')) > 0 \
+                else x.find_all(class_ = 'price-block__final-price finalPrice deal-price-without-discount')[0].get_text())
+            for x in part_frames]
+        else:
+            part_frames = [x for x in soup.find_all(class_ = 'SerpSnippet__data')]
+            part_names = [x.find_all(class_ = 'PartsLink PartsLink_color_black PartsLink_padded SerpSnippet__title')[0].get_text() for x in part_frames]
+            part_links = [x.find_all(class_ = 'PartsLink PartsLink_color_black PartsLink_padded SerpSnippet__title')[0]['href'] for x in part_frames ]
+            part_price = [x.find_all(class_ = 'SerpSnippet__price')[0].get_text() for x in part_frames]
+        return list(zip(part_names, part_links, part_price))
+    except:
+        return []
 #%%
 
+#для тестирования
+req = '{"part_name":"дверца бензобака", "page":0}'
 
-
-
-
-#%%
-website = r'https://baza.drom.ru/sell_spare_parts/?query='
-fullpath = website + urllib.parse.quote('Зеркало')
-loaded_page = urllib.request.urlopen(fullpath)
-loaded_page = loaded_page.read()
-soup = bs4.BeautifulSoup(loaded_page, 'html.parser')
-#%%
-part_names = [x.get_text() for x in soup.find_all(class_ = 'bulletinLink bull-item__self-link auto-shy')]
-part_links = ['https://baza.drom.ru' + x['href'] for x in soup.find_all(class_ = 'bulletinLink bull-item__self-link auto-shy') ]
-part_price = [x.get_text() for x in soup.find_all(class_ = 'price-block__final-price finalPrice tooltip-element tooltip-s')]
-#%%
-part_frames = [x for x in soup.find_all(class_ = 'descriptionCell bull-item-content__cell bull-item-content__description-cell js-description-block')]
-part_names = [x.find_all(class_ = 'bulletinLink bull-item__self-link auto-shy')[0].get_text() for x in part_frames]
-part_links = ['https://baza.drom.ru' + x.find_all(class_ = 'bulletinLink bull-item__self-link auto-shy')[0]['href'] for x in part_frames ]
-part_price = [x.find_all(class_ = 'price-block__price')[0].get_text() \
-    if len(x.find_all(class_ = 'price-block__price')) > 0 \
-        else x.find_all(class_ = 'price-block__final-price finalPrice tooltip-element tooltip-s')[0].get_text() \
-    for x in part_frames]
-#%%
-#%%
-a = part_frames[0].find_all(class_ = 'bulletinLink bull-item__self-link auto-shy')[0].get_text()
-b = part_frames[0].find_all(class_ = 'price-block__price')[0].get_text()
-c 
-#%%
-soup = getpageFromWebsite('https://parts.auto.ru/zapchasti/?isNew=all&text=', 'зеркало')
+get_res = getPriceInfo(req)
 #%%
